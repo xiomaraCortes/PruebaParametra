a1c3edf78055580b1d47ca9b3b7afb4bd44dbf1b
xiomaraCortes <55770691+xiomaraCortes@users.noreply.github.com>
2021-11-26 08:44:55 -0500

    Primer commit

---
+++ src/main/java/com/co/prueba/parametra/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.co.prueba.parametra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
